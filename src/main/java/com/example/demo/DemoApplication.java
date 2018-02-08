7e97b26fe5ee6ea471b1cf9a80327a9ac5eadde0
CLEBER SANTOS SANCHES <7700117056@viavarejo.br>
2018-02-08 16:45:01 -0200

    Atualizações de BCrypt

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,46 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.sanches.security.utils.SenhaUtils;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Value("${paginacao_qtd_por_pagina}")
+	private int qtdPaginas;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("Quantidade de linhas por página: " + qtdPaginas);
+			
+			login();
+		};
+	}
+
+	private void login() {
+
+		String senhaParam = "123456";
+
+		String senhaHash = SenhaUtils.gerarBCriptString(senhaParam);
+		System.out.println(senhaHash);
+
+		String senhaHash2 = SenhaUtils.gerarBCriptString(senhaParam);
+		System.out.println(senhaHash);
+
+		boolean validade = SenhaUtils.senhaValida("123456", senhaHash2);
+
+		System.out.println(validade ? "Senha válida!" : "Senha inválida!");
+
+	}
+
 }
---
+++ src/main/java/com/sanches/security/utils/SenhaUtils.java
@@ -0,0 +1,48 @@
+package com.sanches.security.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class SenhaUtils {
+
+	public SenhaUtils() {		
+	}
+
+	/**
+	 * Gera a senha encriptada.
+	 * 
+	 * @param senhaParam
+	 *            String
+	 * @return String
+	 */
+	public static String gerarBCriptString(String senhaParam) {
+		String senha = senhaParam;
+
+		if (senha != null) {
+			BCryptPasswordEncoder bCryptPassWordEncoder = new BCryptPasswordEncoder();
+			senha = bCryptPassWordEncoder.encode(senha);
+		}
+
+		return senha;
+	}
+
+	/**
+	 * Método para verificar validade da senha
+	 * 
+	 * @param senhaParam
+	 *            String
+	 * @param encodedSenhaParam
+	 *            String
+	 * @return boolean
+	 */
+	public static boolean senhaValida(String senhaParam, String encodedSenhaParam) {
+		boolean retorno = false;
+		String senha = senhaParam;
+		String encodedSenha = encodedSenhaParam;
+		BCryptPasswordEncoder bCryptPassWordEncoder = new BCryptPasswordEncoder();
+
+		retorno = bCryptPassWordEncoder.matches(senha, encodedSenha);
+
+		return retorno;
+	}
+
+}
